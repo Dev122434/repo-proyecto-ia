91f8bf414d373c627c5f5a9dc1b4728301fd0106
Dev122434 <68534leo@universidaddeleon.edu.mx>
2025-12-08 08:36:20 -0600

    Modelo de usuario

---
+++ auth/repository/user_repository.py
@@ -0,0 +1,47 @@
+from auth.entities.user import User
+from sqlmodel import create_engine, SQLModel, Session, select
+import hashlib
+# Configuración de la Base de Datos
+sqlite_file_name = "user.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+# check_same_thread=False es vital para GUIs como Flet
+engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
+
+# Crea las tablas si no existen
+SQLModel.metadata.create_all(engine)
+
+# --- 2. Controlador de Autenticación ---
+class AuthController:
+    def _hash_password(self, password: str) -> str:
+        return hashlib.sha256(password.encode()).hexdigest()
+
+    def registrar_user(self, username, password):
+        with Session(engine) as session:
+            # 1. Verificar si ya existe
+            statement = select(User).where(User.username == username)
+            results = session.exec(statement)
+            if results.first():
+                return False, "El User ya existe"
+            
+            # 2. Crear nuevo User
+            User = User(
+                username=username, 
+                password=self._hash_password(password)
+            )
+            
+            session.add(User)
+            session.commit()
+            return True, "User creado exitosamente"
+
+    def verificar_user(self, username, password):
+        with Session(engine) as session:
+            # Buscar User por nombre
+            statement = select(User).where(User.username == username)
+            User = session.exec(statement).first()
+            
+            # Verificar si existe y si la contraseña coincide
+            if User and User.password == self._hash_password(password):
+                return True, f"Bienvenido, {User.username}"
+            else:
+                return False, "User o contraseña incorrectos"
